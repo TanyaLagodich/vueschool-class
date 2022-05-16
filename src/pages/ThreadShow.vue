6412c9f13e38bfdb78fc011e50c80900b5694451
Tanya Lagodich <tanyalagodichka@gmail.com>
2022-05-16 20:06:01 +0300

    chapter 4: create a post editor

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -2,6 +2,22 @@
      <div class="col-large push-top">
         <h1>{{ thread.title }}</h1>
         <post-list :posts="threadPosts" />
+        <form @submit.prevent="addPost">
+            <div class="form-group">
+                <label for="thread_title">Title:</label>
+                <input type="text" id="thread_title" class="form-input" name="title" >
+            </div>
+
+            <div class="form-group">
+                <label for="thread_content">Content:</label>
+                <textarea v-model="newPostText" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-ghost">Cancel</button>
+                <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+            </div>
+          </form>
     </div>
 </template>
 <script>
@@ -21,7 +37,8 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPostText: ''
     }
   },
   computed: {
@@ -30,6 +47,22 @@ export default {
     },
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'gggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+      }
+      this.posts.push(post)
+      this.threads.posts?.push(postId)
+
+      this.newPostText = ''
     }
   }
 }
