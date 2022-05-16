a1ac16f814937714e7580a62a094e406f972ff11
Tanya Lagodich <tanyalagodichka@gmail.com>
2022-05-16 19:41:24 +0300

    chapter 4: create posts list component

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,42 +1,17 @@
 <template>
      <div class="col-large push-top">
         <h1>{{ thread.title }}</h1>
-        <div class="post-list">
-            <div
-                v-for="postId in thread.posts"
-                :key="postId"
-                class="post"
-            >
-                <div class="user-info">
-                    <a
-                        href="#"
-                        class="user-name"
-                    >
-                        {{ userById(postById(postId).userId).name }}
-                    </a>
-                    <a href="#">
-                        <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-                    </a>
-                    <p class="desktop-only text-small">107 posts</p>
-                </div>
-                <div class="post-content">
-                    <div>
-                    <p>
-                        {{ postById(postId).text }}
-                    </p>
-                    </div>
-                </div>
-                <div class="post-date text-faded">
-                    {{ postById(postId).publishedAt }}
-                </div>
-            </div>
-        </div>
+        <post-list :posts="threadPosts" />
     </div>
 </template>
 <script>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList.vue'
 
 export default {
+  components: {
+    PostList
+  },
   props: {
     id: {
       retuired: true,
@@ -46,26 +21,20 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      posts: sourceData.posts
     }
   },
   computed: {
     thread () {
       return this.threads.find(thread => thread.id === this.id) // this.id is the same this.$route.params.id
-    }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find((p) => p.id === postId)
     },
-    userById (userId) {
-      return this.users.find((u) => u.id === userId)
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
     }
   }
 }
 </script>
-<style scoped>
+<style>
 .post-list {
     margin-top: 20px;
 }
