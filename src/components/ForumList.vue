d31c011989273ae77ae7f7d97016ecabcbed6980
Tanya Lagodich <tanyalagodichka@gmail.com>
2022-05-24 19:15:13 +0300

    creating a forum list component

---
+++ src/components/ForumList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-full">
+      <div class="forum-list">
+        <h2 class="list-title">Forums</h2>
+
+        <div
+            class="forum-listing"
+            v-for="forum in forums"
+            :key="forum.id"
+            >
+        <div class="forum-details">
+            <router-link
+            class="forum-name"
+            :to="{ name: 'Forum', params: { id: forum.id } }"
+            >
+            {{ forum.name }}
+            </router-link>
+            <p class="forum-description ">{{ forum.description }}</p>
+        </div>
+        <div class="threads-count">
+            <p class="count text-lead">{{ forum.threads?.length }}</p>
+            {{ forumThreadWord(forum) }}
+        </div>
+        <div class="last-thread">
+        </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    forums: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    forumThreadWord (forum) {
+      if (forum.threads?.length) {
+        if (forum.threads.length > 1) return 'threads'
+        return 'thread'
+      }
+      return 'no threads'
+    }
+  }
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <h1>Welcome to the Forum!</h1>
-  <thread-list :threads="threads" />
+  <forum-list :forums="forums" />
 </template>
 <script>
 import sourceData from '@/data.json'
-import ThreadList from '@/components/ThreadList'
+import ForumList from '@/components/ForumList'
 
 export default {
   components: {
-    ThreadList
+    ForumList
   },
   data () {
     return {
-      threads: sourceData.threads
+      forums: sourceData.forums
     }
   }
 }
